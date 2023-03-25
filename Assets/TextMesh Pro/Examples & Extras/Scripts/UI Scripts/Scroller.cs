8cbb8b3486976fd0bcfade86221b90f83dd2f6f6
exekutor7 <122258608+exekutor7@users.noreply.github.com>
2023-03-25 14:31:54 +0100

    UI and shaders
    Added 2 scenes, UI and shaders
    health system

---
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/UI Scripts/Scroller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] private RawImage rawImg;
+    [SerializeField] private float _x, _y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rawImg.uvRect = new Rect(rawImg.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, rawImg.uvRect.size);
+    }
+}
