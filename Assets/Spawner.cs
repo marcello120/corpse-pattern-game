fa4d1f5a3f25ebe917621ee5b9ac9ff74afa200f
marcello120 <sarosi.marcell@gmail.com>
2023-12-12 18:29:52 +0100

    Proper Spawning
    Can now spawn items within a pos's radius without colliding with existing obstacles
    also kill me

---
+++ Assets/Spawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject thingToSpawn;
+    public CircleCollider2D circleCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
