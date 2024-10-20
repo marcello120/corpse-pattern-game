4341fa6f7f66cb6cb05e89d580ecad50cc98d589
marcello120 <sarosi.marcell@gmail.com>
2024-10-20 20:45:33 +0200

    better small snake movement
    better small snake movement

--- Assets/SnakeEnemy.cs
+++ Assets/SnakeEnemy.cs
@@ -7,7 +7,7 @@ public class SnakeEnemy : Enemy
     // Start is called before the first frame update
     void Start()
     {
-        float random = UnityEngine.Random.Range(0.75f, 1.5f);
+        float random = UnityEngine.Random.Range(0.5f, 0.75f);
         movemetSpeed *= random;
         base.Init();
         setState(State.Moving);
