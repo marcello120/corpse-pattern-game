9526d3a44e4403c4a0e50aada0f50d559b771171
marcello120 <sarosi.marcell@gmail.com>
2024-10-27 00:39:49 +0200

    touches
    not final touches, just touches

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void Resume()
     {
-        
+        RiggedPlayerController player = (RiggedPlayerController)FindFirstObjectByType(typeof(RiggedPlayerController));
+        player.Resume();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
