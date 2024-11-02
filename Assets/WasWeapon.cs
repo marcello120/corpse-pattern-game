9f4a39264b886b7436b27369376bac244b67c8f5
marcello120 <sarosi.marcell@gmail.com>
2024-11-03 00:53:46 +0100

    Add more sounds
    Add more sounds

--- Assets/WasWeapon.cs
+++ Assets/WasWeapon.cs
@@ -66,4 +66,41 @@ public class WasWeapon : Weapon
         
     }
 
+    public override void HeavyAttack()
+    {
+        animator.SetTrigger("Attack");
+
+
+        if (canAttack)
+        {
+            Transform playerTransform = weaponBody.transform;
+            float offset = reach;
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 attackDirection = (mousePosition - (Vector2)playerTransform.position).normalized;
+            Vector2 attackPosition = (Vector2)playerTransform.position + attackDirection * offset;
+
+            // Calculate rotation to face the mouse position
+            Vector2 direction = mousePosition - (Vector2)playerTransform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            swing.InitWeaponAttack(weaponKnockback, weaponAttackPower);
+
+            WeaponSwing effect = Instantiate(swing, transform);
+            effect.transform.localScale *= size * 25;
+
+            //comment if tired
+            // effect.transform.SetParent(transform, true);
+
+            attackQueued = false;
+            canAttack = false;
+        }
+        else
+        {
+            attackQueued = true;
+        }
+
+    }
+
 }
